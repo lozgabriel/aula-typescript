e405935fa5ed8fb9dac8992b92d4386e01388a27
lozgabriel <lozgabriel@gmail.com>
2018-01-12 15:42:10 -0200

    ECMA6

---
+++ app2.js
@@ -0,0 +1,67 @@
+var Carro = (function () {
+    function Carro(modelo, numeroDePortas) {
+        this.velocidade = 0;
+        this.modelo = modelo;
+        this.numeroDePortas = numeroDePortas;
+    }
+    Carro.prototype.acelerar = function () {
+        this.velocidade = this.velocidade + 10;
+    };
+    Carro.prototype.parar = function () {
+        this.velocidade = 0;
+    };
+    Carro.prototype.velocidadeAtual = function () {
+        return this.velocidade;
+    };
+    return Carro;
+}());
+var Concessionaria = (function () {
+    function Concessionaria(endereco, listaDeCarros) {
+        this.endereco = endereco;
+        this.listaDeCarros = listaDeCarros;
+    }
+    Concessionaria.prototype.fornecerEndereco = function () {
+        return this.endereco;
+    };
+    Concessionaria.prototype.mostrarListaDeCarros = function () {
+        return this.listaDeCarros;
+    };
+    return Concessionaria;
+}());
+var Pessoa = (function () {
+    function Pessoa(nome, carroPreferido) {
+        this.nome = nome;
+        this.carroPreferido = carroPreferido;
+    }
+    Pessoa.prototype.dizerNome = function () {
+        return this.nome;
+    };
+    Pessoa.prototype.dizerCarroPreferido = function () {
+        return this.carroPreferido;
+    };
+    Pessoa.prototype.comprarCarro = function (carro) {
+        this.carro = carro;
+    };
+    Pessoa.prototype.dizerCarroQueTem = function () {
+        return this.carro;
+    };
+    return Pessoa;
+}());
+/* --- criar carros ---*/
+var carroA = new Carro('dodge journey', 4);
+var carroB = new Carro('veloster', 3);
+var carroC = new Carro('cerato', 4);
+/* --- montar a lista de carros da concessionaria ---*/
+var listaDeCarros = [carroA, carroB, carroC];
+var concessionaria = new Concessionaria('Av Paulista', listaDeCarros);
+/* --- exibir a lista de carros --- */
+//console.log(concessionaria.mostrarListaDeCarros())
+/* --- comprar o carro ---*/
+var cliente = new Pessoa('João', 'veloster');
+concessionaria.mostrarListaDeCarros().map(function (carro) {
+    if (carro['modelo'] == cliente.dizerCarroPreferido()) {
+        //compra o carro
+        cliente.comprarCarro(carro);
+    }
+});
+console.log(cliente.dizerCarroQueTem());
